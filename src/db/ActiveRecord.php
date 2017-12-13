e8f2d75dc6a990ab5a51bb79764e0a39f02f4c99
Artyom Kravchenko <lan143@lan143.ru>
2017-12-13 22:46:16 +0300

    Все поля safe по умолчанию в ActiveRecord

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -4,5 +4,17 @@ namespace bulldozer\db;
 
 class ActiveRecord extends \yii\db\ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        $rules = [];
 
+        foreach ($this->getAttributes() as $attribute) {
+            $rules[] = [$attribute, 'safe'];
+        }
+
+        return $rules;
+    }
 }
